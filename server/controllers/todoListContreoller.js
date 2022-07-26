ba8183407d282ea98d1be614e2ea70adca153ef4
Meowmerry <meow3525@gmail.com>
2022-07-26 18:07:07 -0500

    create getAllList

--- server/controllers/todoListContreoller.js
+++ server/controllers/todoListContreoller.js
@@ -1,30 +1,48 @@
+const { query } = require("express");
 const db = require("../models/todolist.model");
 const createError = require("../utils/constants");
 
-const todolistController = {}
+const todolistController = {};
 
-todolistController.createItemList = async(req, res, next)=>{
-    try{
-        const {id, items,isDone,minutes} = req.body;
-        const sqlQuery = `INSERT INTO listTodo
+todolistController.createItemList = async (req, res, next) => {
+  try {
+    const { id, items, isDone, minutes } = req.body;
+    const sqlQuery = `INSERT INTO listTodo
         (id, items, isDone, minutes)
         VALUES($1, $2, $3, $4);`;
-        await db.query(sqlQuery,[id, items, isDone, minutes])
-        return next()
-    }catch (error) {
-        return next(createError({ message: { err: error.message } }));
-      }
+    await db.query(sqlQuery, [id, items, isDone, minutes]);
+    res.locals.status = "Successfully created!";
+    return next();
+  } catch (error) {
+    return next(createError({ message: { err: error.message } }));
+  }
+};
 
-}
-todolistController.getAllList = async(req, res, next) =>{
+todolistController.getAllList = async (req, res, next) => {
+  try {
+    const sqlQuery = "SELECT * From listTodo"
+    const data = await db.query(sqlQuery);
+    res.locals.list = data.rows;
+    return next();
+  } catch (error) {
+    return next(createError({ message: { err: error.message } }));
+  }
+};
 
-}
+todolistController.updateListById = async (req, res, next) => {
+  try {
 
-todolistController.updateListById = async(req, res, next) =>{
+    res.locals.status = "Successfully updated!";
+    return next();
+  } catch (error) {
+    return next(createError({ message: { err: error.message } }));
+  }
+};
 
-}
-
-todolistController.deleteListById = async(req, res, next) =>{
-
-}
+todolistController.deleteListById = async (req, res, next) => {
+  try {
+  } catch (error) {
+    return next(createError({ message: { err: error.message } }));
+  }
+};
 module.exports = todolistController;
--- server/routes/api.js
+++ server/routes/api.js
@@ -3,11 +3,21 @@ const router = express.Router();
 const todolistController = require('../controllers/todoListContreoller')
 
 router.post('/createList',todolistController.createItemList,(req, res)=>{
-    return res.status(200).json("Create Success")
+    return res.status(200).json(res.locals.status)
+});
+
+router.get('/lists',todolistController.getAllList ,(req, res)=>{
+    return res.status(200).json({list: res.locals.list})
+});
+
+router.patch('/list/:id',todolistController.updateListById ,(req, res)=>{
+    return res.status(200).json(res.locals.status)
 })
 
-router.get('/list',todolistController.getAllList,(req, res)=>{
-    return res.status(200).json({})
+router.delete('/list/:id',todolistController.deleteListById ,(req, res)=>{
+    return res.status(200).json({list: res.locals.list})
 })
+
+
 
 module.exports = router;
